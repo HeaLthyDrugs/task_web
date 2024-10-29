0f196e3dfe0ad104a51347980b7282b6de46b200
Manish Vishwakarma <122515021+HeaLthyDrugs@users.noreply.github.com>
2024-10-29 21:39:16 +0530

    Loading states functionality done

--- components/download/index.tsx
+++ components/download/index.tsx
@@ -4,6 +4,8 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { FiDownload } from 'react-icons/fi';
 import { BackgroundGradient } from '../ui/background-gradient';
+import { MultiStepLoader as Loader } from '../ui/multi-step-loader';
+import { IconSquareRoundedX } from '@tabler/icons-react';
 
 interface DownloadButtonProps {
   text?: string;
@@ -11,11 +13,55 @@ interface DownloadButtonProps {
 }
 
 export default function DownloadButton({ text = "Download Now", href }: DownloadButtonProps) {
+  const [loading, setLoading] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+
+  const loadingStates = [
+    {
+      text: "Checking off our checklist...",
+    },
+    {
+      text: "Organizing digital sticky notes...",
+    },
+    {
+      text: "Decluttering your future...",
+    },
+    {
+      text: "Finding lost to-dos under the couch...",
+    },
+    {
+      text: "Aligning tasks perfectly...",
+    },
+    {
+      text: "Scheduling productivity boosts...",
+    },
+    {
+      text: "Making procrastination disappear...",
+    },
+    {
+      text: "Adding extra hours to your day...",
+    },
+    {
+      text: "Mastering the art of getting things done...",
+    },
+    {
+      text: "Welcome to your organized life! ✓",
+    },
+  ];
 
   return (
     <div className="fixed top-4 right-4 z-50">
-      <a href={href} target="_blank" rel="noopener noreferrer">
+      <Loader loadingStates={loadingStates} loading={loading} duration={2000} />
+
+      {loading && (
+        <button
+          className="fixed top-5 right-5 text-black dark:text-white z-[120]"
+          onClick={() => setLoading(false)}
+        >
+          <IconSquareRoundedX className="h-10 w-10" />
+        </button>
+      )}
+      <button rel="noopener noreferrer" onClick={() => setLoading(true)}>
         <BackgroundGradient className="relative inline-flex items-center gap-2 px-3 py-3 overflow-hidden rounded-full text-white font-medium group" >
           <motion.div
             className="relative z-10"
@@ -23,7 +69,7 @@ export default function DownloadButton({ text = "Download Now", href }: Download
             <FiDownload size={20} />
           </motion.div>
         </BackgroundGradient>
-      </a>
+      </button>
     </div>
   );
 }
